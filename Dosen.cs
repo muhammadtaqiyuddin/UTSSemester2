fae7478de209fe9fc34784714b13ae4117e11bab
Taqi <mtaqiyuddin101@gmail.com>
2022-05-24 11:29:09 +0700

    utssemester2

---
+++ Dosen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Uts
+{
+    class Dosen
+    {
+        public int id;
+        public string name;
+        public int nik;
+        public string gender;
+        public string course;
+
+        public void defaultDosen()
+        {
+            Console.WriteLine(
+                "ID     : 1\nNAMA   : Bayu Nadya Kusuma S.T, M.Eng\nNIK    : 0505028902\nGENDER : PRIA\nCOURSE : PEMROGRAMAN \n"
+            );
+            Console.WriteLine(
+                "ID     : 2\nNAMA   : Arif Akabarul Huda, S.Si, M.Eng\nNIK    : 0506058901\nGENDER : PRIA\nCOURSE : PEMROGRAMAN \n"
+            );
+            Console.WriteLine(
+                "ID     : 3\nNAMA   : M. Fairul Filza, M.Kom\nNIK    : 0504098603\nGENDER : PRIA\nCOURSE : PEMROGRAMAN \n"
+            );
+            Console.WriteLine(
+                "ID     : 4\nNAMA   : Kamarudin, M.Kom\nNIK    : 0504098603\nGENDER : PRIA\nCOURSE : PEMROGRAMAN \n"
+            );
+        } 
+        public void attDosen()
+        {
+            Console.WriteLine("TAMBAHKAN NAMA DOSEN : ");
+            name = (Console.ReadLine());
+            Console.WriteLine("MASUKKAN GENDER DOSEN : ");
+            gender = (Console.ReadLine());
+        }
+        public void addCourse()
+        {
+            Console.WriteLine("MASUKKAN COURSE DOSEN : ");
+             course = (Console.ReadLine());
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace Uts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dosen objDosen = new Dosen();
+            Console.WriteLine("\n========== DEFAULT DOSEN ==========\n");
+            objDosen.defaultDosen();
+            Console.WriteLine("\n========== TAMBAHAN DOSEN ==========\n");
+            objDosen.attDosen();
+            objDosen.addCourse();
+            Console.WriteLine("DATA DOSEN BERHASIL DITAMBAHKAN!!!");
+            objDosen.defaultDosen();
+            Console.WriteLine("ID     : 5\nNAMA   : " + objDosen.name +
+                "\nNIK    : 524119202\nGENDER : " + objDosen.gender +
+                "\nCOURSE : " + objDosen.course) ;
+           
+        }
+    }
+}
